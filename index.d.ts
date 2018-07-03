af887074ceda4690b9db2afb70e884ecd16864be
Hao Luo <howlowck@gmail.com>
2018-07-03 15:00:26 -0500

    fixed bind error

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,39 @@
 import { Packet } from "mqtt"
+import { Message } from "rhea/typings/message";
+import { Connection, Container, Receiver, Session } from "rhea";
+
+interface AmqpContext {
+  connection: Connection
+  container: Container
+  message: Message
+  receiver: Receiver
+  session: Session
+}
+
+/**
+ * Publishes the event
+ * @param message 
+ * @param appParams 
+ */
+function publish(message: string, appParams: {[key: string]: any}) : void
+
+/**
+ * Closes the connection
+ * @return void
+ */
+function close() : void
+
+/**
+ * Connect to Created Device on IotHub
+ * @param deviceConnectionString Device Connection String (not the iothub connection string)
+ * @param onC2DMessage
+ */
+export function connectDevice(deviceConnectionString: string, onC2DMessage: (topic: string, message: Buffer, packet: Packet) => void) : {publish, close}
 
 /**
  * 
+ * @param eventHubName Eventhub-compatible name (under iothub-> endpoints -> events)
+ * @param eventHubConnectionString Eventhub-compatible Connection String (under iothub-> endpoints -> events)
+ * @param onMessage 
  */
-
- function publish(message: string, appParams: {[key: string]: any}) : void
- function close() : void
-
- export function connectDevice(deviceConnectionString: string, onC2DMessage: (topic: string, message: Buffer, packet: Packet) => void) : {publish, close}
- export function monitorEvents(eventHubName: string, eventHubConnectionString: string, onMessage: (message: string, context: any) => void): {close}
+export function monitorEvents(eventHubName: string, eventHubConnectionString: string, onMessage: (message: string, context: AmqpContext) => void): {close}
